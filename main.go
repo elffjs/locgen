757320fd600693bc3944be05ca1b4f32167041d5
Dylan Moreland <dylan@dimo.zone>
2025-08-20 13:50:19 -0400

    Reorg

--- main.go
+++ main.go
@@ -12,16 +12,13 @@ import (
 
 const fieldCoordinates = "currentLocationCoordinates" // TODO(elffjs): Move this to the vss package.
 
+// zeroTime is used to reset the timestamp on the coordinate store.
+var zeroTime time.Time
+
 const (
 	allowedLocationGap = 500 * time.Millisecond
 	dropSignalName     = "__drop"
 )
-
-func fmtTime(t time.Time) string {
-	return t.Format(time.RFC3339)
-}
-
-var zeroTime time.Time
 
 // ProcessSignals transforms a slice of input signals in ways that
 // simplify downstream processing. Currently this means:
@@ -42,124 +39,176 @@ var zeroTime time.Time
 // The returned slice of signals is always meaningful, even if an error
 // is also returned.
 //
-// Note that this function does reorder the input slice.
+// Note that this function may reorder the input slice.
 func ProcessSignals(signals []vss.Signal) ([]vss.Signal, error) {
-	if len(signals) == 0 {
-		return signals, nil
+	store := newStore(signals)
+	return store.processSignals()
+}
+
+func newStore(signals []vss.Signal) *coordinateStore {
+	return &coordinateStore{
+		signals:  signals,
+		lastLat:  -1,
+		lastLon:  -1,
+		lastHDOP: -1,
 	}
+}
 
-	// Used to populate the payload-wide stuff, like token id or
-	// CloudEvent id, in newly created rows. We assume that these do
-	// not change within a single payload.
-	template := signals[0]
+type coordinateStore struct {
+	// lastLat is the index of the signals slice holding latitude for
+	// the location triple under construction. If there is no latitude
+	// yet found for the triple then the value of lastLat is -1.
+	lastLat int
+	// lastLon is like lastLat for longitude.
+	lastLon int
+	// lastHDOP is like lastLat for HDOP.
+	lastHDOP int
+	// lastTime is the timestamp of the earliest signal in the active
+	// triple. If we have no parts for the active triple then this
+	// will be the zero value of time.Time.
+	lastTime time.Time
 
-	var errs error
-	var created []vss.Signal
-	var lastTime time.Time
-	lastLat := -1
-	lastLon := -1
-	lastHDOP := -1
+	// signals is the input slice of signals.
+	signals []vss.Signal
 
-	tryCreateLocationSignal := func() {
-		var loc vss.Location
-		var create bool
+	// created holds location signals that we've constructed while
+	// iterating over signals.
+	created []vss.Signal
+	// errs contains errors arising from location construction.
+	// Typically these have to do with unpaired coordinates, or
+	// latitude = longitude = 0.
+	errs []error
+}
 
-		if lastLat != -1 && lastLon != -1 {
-			lat := signals[lastLat].ValueNumber
-			lon := signals[lastLon].ValueNumber
-
-			if lat == 0 && lon == 0 {
-				signals[lastLat].Name = dropSignalName
-				signals[lastLon].Name = dropSignalName
-				errs = errors.Join(errs, fmt.Errorf("latitude and longitude at origin at time %s", fmtTime(lastTime)))
-			} else {
-				loc.Latitude = lat
-				loc.Longitude = lon
-				create = true
-			}
-		} else if lastLat != -1 {
-			signals[lastLat].Name = dropSignalName
-			errs = errors.Join(errs, fmt.Errorf("unpaired latitude at time %s", fmtTime(lastTime)))
-		} else if lastLon != -1 {
-			signals[lastLon].Name = dropSignalName
-			errs = errors.Join(errs, fmt.Errorf("unpaired longitude at time %s", fmtTime(lastTime)))
-		}
-
-		if lastHDOP != -1 {
-			loc.HDOP = signals[lastHDOP].ValueNumber
-			create = true
-		}
-
-		if create {
-			created = append(created, vss.Signal{
-				TokenID:       template.TokenID,
-				Timestamp:     lastTime,
-				Name:          fieldCoordinates,
-				ValueLocation: loc,
-				Source:        template.Source,
-				Producer:      template.Producer,
-				CloudEventID:  template.CloudEventID,
-			})
-		}
-
-		lastLat = -1
-		lastLon = -1
-		lastHDOP = -1
-		lastTime = zeroTime
+func (c *coordinateStore) processSignals() ([]vss.Signal, error) {
+	if len(c.signals) == 0 {
+		return c.signals, nil
 	}
 
 	// Sorting this way makes it easier to handle time gaps. Sorting
-	// thereafter by name is not strictly necessary.
-	slices.SortFunc(signals, func(a, b vss.Signal) int {
+	// thereafter by name is not strictly necessary. Typically, this
+	// sorting will already have been performed upstream by a
+	// duplicate detector.
+	slices.SortFunc(c.signals, func(a, b vss.Signal) int {
 		return cmp.Or(a.Timestamp.Compare(b.Timestamp), cmp.Compare(a.Name, b.Name))
 	})
 
-	for i, sig := range signals {
-		if !lastTime.IsZero() && sig.Timestamp.Sub(lastTime) >= allowedLocationGap {
-			tryCreateLocationSignal()
-		}
-
-		// This logic could be made shorter and less repetitive by
-		// playing around with *int.
-		switch sig.Name {
-		case vss.FieldCurrentLocationLatitude:
-			if lastLat != -1 {
-				// Start a new triple, but see if what's already being
-				// tracked is enough to yield a row.
-				tryCreateLocationSignal()
-			}
-			lastLat = i
-		case vss.FieldCurrentLocationLongitude:
-			if lastLon != -1 {
-				tryCreateLocationSignal()
-			}
-			lastLon = i
-		case vss.FieldDIMOAftermarketHDOP:
-			if lastHDOP != -1 {
-				tryCreateLocationSignal()
-			}
-			lastHDOP = i
-		default:
-			continue
-		}
-
-		if lastTime.IsZero() {
-			lastTime = sig.Timestamp
-		}
+	for i := range c.signals {
+		c.processSignal(i)
 	}
 
 	// One last attempt, in case we're in the process of constructing
 	// a location.
-	tryCreateLocationSignal()
+	c.tryCreateLocation()
 
 	var out []vss.Signal
-	for _, sig := range signals {
+	for _, sig := range c.signals {
 		if sig.Name != dropSignalName {
 			out = append(out, sig)
 		}
 	}
 
-	out = append(out, created...)
+	out = append(out, c.created...)
 
-	return out, errs
+	return out, errors.Join(c.errs...)
+}
+
+func (c *coordinateStore) processSignal(index int) {
+	sig := c.signals[index]
+
+	if !c.lastTime.IsZero() && sig.Timestamp.Sub(c.lastTime) >= allowedLocationGap {
+		c.tryCreateLocation()
+	}
+
+	// This logic could be made shorter and less repetitive by
+	// playing around with *int.
+	switch sig.Name {
+	case vss.FieldCurrentLocationLatitude:
+		if c.lastLat != -1 {
+			// Start a new triple, but see if what's already being
+			// tracked is enough to yield a row.
+			c.tryCreateLocation()
+		}
+		c.lastLat = index
+	case vss.FieldCurrentLocationLongitude:
+		if c.lastLon != -1 {
+			c.tryCreateLocation()
+		}
+		c.lastLon = index
+	case vss.FieldDIMOAftermarketHDOP:
+		if c.lastHDOP != -1 {
+			c.tryCreateLocation()
+		}
+		c.lastHDOP = index
+	default:
+		return
+	}
+
+	if c.lastTime.IsZero() {
+		c.lastTime = sig.Timestamp
+	}
+}
+
+// tryCreateLocation tries to add a VSS location row using the active
+// location triple.
+//
+// Only call this function when forced: if there is any chance that
+// the triple can be completed by the next element of the slice then
+// calling this function may discard the elements of the active triple
+// on the grounds of being incomplete.
+func (c *coordinateStore) tryCreateLocation() {
+	var loc vss.Location
+	var create bool
+
+	template := c.signals[0]
+
+	if c.lastLat != -1 && c.lastLon != -1 {
+		lat := c.signals[c.lastLat].ValueNumber
+		lon := c.signals[c.lastLon].ValueNumber
+
+		if lat == 0 && lon == 0 {
+			c.signals[c.lastLat].Name = dropSignalName
+			c.signals[c.lastLon].Name = dropSignalName
+			c.errs = append(c.errs, fmt.Errorf("latitude and longitude at origin at time %s", fmtTime(c.lastTime)))
+		} else {
+			loc.Latitude = lat
+			loc.Longitude = lon
+			create = true
+		}
+	} else if c.lastLat != -1 {
+		c.signals[c.lastLat].Name = dropSignalName
+		c.errs = append(c.errs, fmt.Errorf("unpaired latitude at time %s", fmtTime(c.lastTime)))
+	} else if c.lastLon != -1 {
+		c.signals[c.lastLon].Name = dropSignalName
+		c.errs = append(c.errs, fmt.Errorf("unpaired longitude at time %s", fmtTime(c.lastTime)))
+	}
+
+	if c.lastHDOP != -1 {
+		loc.HDOP = c.signals[c.lastHDOP].ValueNumber
+		create = true
+	}
+
+	if create {
+		c.created = append(c.created, vss.Signal{
+			TokenID:       template.TokenID,
+			Timestamp:     c.lastTime,
+			Name:          fieldCoordinates,
+			ValueLocation: loc,
+			Source:        template.Source,
+			Producer:      template.Producer,
+			CloudEventID:  template.CloudEventID,
+		})
+	}
+
+	c.lastLat = -1
+	c.lastLon = -1
+	c.lastHDOP = -1
+	c.lastTime = zeroTime
+}
+
+// fmtTime formats the given time per RFC-3339, for use in errors
+// returned to the client. The default Go format used for %s is not
+// standard.
+func fmtTime(t time.Time) string {
+	return t.Format(time.RFC3339)
 }
